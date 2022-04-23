1bb6138ea9d894fd0499f785b1b2ec1f984a8b54
ENGO150 <v.smejkal06@gmail.com>
2022-04-23 18:41:51 +0200

    moved repository

--- include/misc.h
+++
@@ -1,7 +0,0 @@
-#ifndef WHY2_MISC_H
-#define WHY2_MISC_H
-
-void checkVersion(); //THIS FUNCTION CHECKS IF LATEST VERSION OF WHY2 IS USED
-int countIntLength(int number); //RETURNS LENGTH OF number
-
-#endif
